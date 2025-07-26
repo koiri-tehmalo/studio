7be905b83d1ff50bc721fb64fed793a9520281df
มูฮําหมัดคอยรี เต๊ะมาลอ <khayreetehmalo@gmail.com>
2025-07-26 07:06:38 +0000

    ในหน้าประวัติการสังเกตการณ์ มีexport default function DashboardLayout({

--- src/app/dashboard/history/layout.tsx
+++ src/app/dashboard/history/layout.tsx
@@ -1,17 +1,11 @@
 'use client';
 
 import React from 'react';
-import DashboardLayout from '@/app/dashboard/layout';
-
 
 export default function HistoryLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <DashboardLayout>
-        {children}
-    </DashboardLayout>
-  );
+  return <>{children}</>;
 }
